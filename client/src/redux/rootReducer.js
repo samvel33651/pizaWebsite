1310c98583e3d0d2fdeda53a9a1cacd28d813bf0
V Samvel <samvel33651@gmail.com>
2020-07-07 18:46:30 +0400

    smnall  fix

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,9 +1,8 @@
 import {combineReducers} from "redux";
 import productsReducer from './products/reducer';
 import userReducer from './user/reducer';
-import { reducer as notifReducer } from 'redux-notifications';
+
 export  const  rootReducer = combineReducers({
     products: productsReducer,
     userData: userReducer,
-    notifs: notifReducer,
 });
